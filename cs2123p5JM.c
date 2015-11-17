cd1bb187775e9e8475aa7d99d37dd22ca2548cdd
Justin Mungal <justin@n0de.ws>
2015-11-17 15:23:56 -0600

    added deleteItem

--- cs2123p5JM.c
+++ cs2123p5JM.c
@@ -1,9 +1,53 @@
-//
-//  cs2123p5JM.c
-//  p5
-//
-//  Created by Justin Mungal on 11/17/15.
-//  Copyright © 2015 Justin Mungal. All rights reserved.
-//
+/******************************************************************************
+ cs2123p5JM.c by Justin Mungal
+ 
+ Purpose:
+  -Defines deleteItem and findParent functions
+ 
+ Command Parameters:
+ n/a
+ Input:
+ 
+ Results:
+ 
+ Notes:
+ *******************************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p5.h"
+
+void deleteItem(Tree tree, char szId[])
+{
+    NodeT *p;
+    NodeT *pParent;
+    
+    p = findId(tree->pRoot, szId);
+    
+    //return if the node is null
+    if (p == NULL)
+    {
+        printf("ERROR ID %s NOT FOUND\n", szId);
+        return;
+    }
+    
+    //recurse into child nodes
+    deleteItem(tree, p->pChild->element.szId);
+    
+    //set parent pointer to null
+    //insert call to find parent here
+    //set parent->pChild value to null 
+    
+    //delete node
+    free(p);
+    
+}
+
+NodeT *findParent(NodeT *pParent, NodeT *p, NodeT *pkid)
+{
+    
+}
